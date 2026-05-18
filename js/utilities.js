1ea37fba432f18d13162113fca256b58bf6f5d6d
Kouki <sachietokugawa@gmail.com>
2026-05-17 23:29:58 -0500

    Fix utilities logger

--- js/utilities.js
+++ js/utilities.js
@@ -263,7 +263,7 @@ const DOMUtil = (() => {
     showError, clearError,
   };
 })();
-window.Logger = Logger();
-window.StorageUtil = StorageUtil();
-window.Validators = Validators();
-window.DOMUtil = DOMUtil();
+window.Logger = Logger;
+window.StorageUtil = StorageUtil;
+window.Validators = Validators;
+window.DOMUtil = DOMUtil;
